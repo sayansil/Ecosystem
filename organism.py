2060f370dfbc847a01cbc9b7742e309dbe8eff1c
Sayan Sil <sayan227sil@ymail.com>
2018-12-28 04:54:24 +0530

    Add increment/decrement vitality & stamina

--- organism.py
+++ organism.py
@@ -3,9 +3,7 @@ class herbivore:
     def __init__(
         self, 
         chromosome, 
-        generation, 
-        age,
-        fitness):
+        generation):
         self.max_age = 30
         self.chromosome = chromosome
         self.generation = generation
@@ -20,7 +18,7 @@ class herbivore:
         self.speed = self.max_speed_at_age
         self.max_appetite_at_age = self.get_base_appetite()
         self.appetite = self.max_appetite_at_age
-        self.fitness = fitness
+        self.fitness = 0.0
         self.chromosome_number = 256
         self.age_death_index = 5
         self.fitness_death_index = 100
@@ -35,91 +33,63 @@ class herbivore:
         self.stamina_on_appetite = -0.3
 
     def get_base_vitality(self):
+        # TODO
         return 123
 
     def get_base_stamina(self):
+        # TODO
         return 123
         
     def get_vitality_multiplier(self):
+        # TODO
         return 123
 
     def get_stamina_multiplier(self):
+        # TODO
         return 123
 
     def get_max_height(self):
+        # TODO
         return 123
 
     def get_base_height(self):
+        # TODO
         return 123
 
     def get_height_multiplier(self):
+        # TODO
         return 123
 
     def get_max_weight(self):
+        # TODO
         return 123
 
     def get_base_weight(self):
+        # TODO
         return 123
 
     def get_weight_multiplier(self):
+        # TODO
         return 123
 
     def get_immunity(self):
+        # TODO
         return 123
 
     def get_base_speed(self):
+        # TODO
         return 123
 
     def get_max_speed(self):
+        # TODO
         return 123
 
     def get_base_appetite(self):
+        # TODO
         return 123
-
-    def get_chromosome(self):
-        return self.chromosome
-
-    def get_traits(self):
-        return self.traits
-
-    def get_generation(self):
-        return self.generation
-
-    def get_age(self):
-        return self.age
-
-    def get_fitness(self):
-        return self.fitness
-
-    def get_vitality(self):
-        return self.vitality
-
-    def get_stamina(self):
-        return self.stamina
         
     def get_gender(self):
         return self.chromosome[self.chromosome_number - 1]
-
-    def set_chromosome(self, chromosome):
-        self.chromosome = chromosome
-
-    def set_traits(self, traits):
-        self.traits = traits
-
-    def set_generation(self, generation):
-        self.generation = generation
-
-    def set_age(self, age):
-        self.age = age
-        
-    def set_vitality(self, vitality):
-        self.vitality = vitality
-
-    def set_stamina(self, stamina):
-        self.stamina = stamina
-
-    def set_fitness(self, fitness):
-        self.fitness = fitness
 
     def increment_age(self):
         self.age = self.age + 1
@@ -133,12 +103,10 @@ class herbivore:
             self.get_max_weight())
         self.max_vitality_at_age = self.get_base_vitality() * (
             self.get_vitality_multiplier() *
-            np.power(np.e, -(self.age - self.max_age*0.5)/(self.max_age * self.get_base_vitality)) + 1
-        )
+            np.power(np.e, -(self.age - self.max_age*0.5)/(self.max_age * self.get_base_vitality)) + 1)
         self.max_stamina_at_age = self.get_base_stamina() * (
             self.get_stamina_multiplier() *
-            np.power(np.e, -(self.age - self.max_age*0.5)/(self.max_age * self.get_base_stamina)) + 1
-        )
+            np.power(np.e, -(self.age - self.max_age*0.5)/(self.max_age * self.get_base_stamina)) + 1)
         self.max_vitality_at_age = self.max_vitality_at_age * (1 + 
             self.height_on_vitality * self.height/self.get_max_height() + 
             self.weight_on_vitality * self.weight/self.get_max_weight())
@@ -150,9 +118,27 @@ class herbivore:
         self.max_speed_at_age = self.max_speed_at_age * (1 +
             self.height_on_speed * self.height/self.get_max_height() + 
             self.weight_on_speed * self.weight/self.get_max_weight())
-        self.max_appetite_at_age = (1-self.get_base_appetite())*np.power(np.e, (-1/np.power(self.max_age, 1.5) * np.power(x - self.max_age/3, 2))) + self.get_base_appetite()
-        self.max_appetite_at_age = self.max_appetite_at_age * (1 + 
-            self.vitality_on_appetite * self.vitality/self.max_vitality_at_age +
+        self.max_appetite_at_age = self.get_base_appetite() + (1-self.get_base_appetite()) * np.power(np.e, 
+            (-1/np.power(self.max_age, 1.5)) * np.power(self.age - self.max_age/3, 2))
+
+    def increment_vitality(self):
+        self.vitality = np.minimum(self.vitality + 1, self.max_vitality_at_age)
+        self.appetite = self.appetite * (1 + 
+            self.vitality_on_appetite * self.vitality/self.max_vitality_at_age)
+
+    def decrement_vitality(self):
+        self.vitality = np.maximum(self.vitality - 1, 0)
+        self.appetite = self.appetite * (1 + 
+            self.vitality_on_appetite * self.vitality/self.max_vitality_at_age)
+
+    def increment_stamina(self):
+        self.stamina = np.minimum(self.stamina + 1, self.max_stamina_at_age)
+        self.appetite = self.appetite * (1 +
+            self.stamina_on_appetite * self.stamina/self.max_stamina_at_age)
+
+    def decrement_stamina(self):
+        self.stamina = np.maximum(self.stamina - 1, 0)
+        self.appetite = self.appetite * (1 +
             self.stamina_on_appetite * self.stamina/self.max_stamina_at_age)
 
     def will_die_of_age(self):
@@ -165,5 +151,4 @@ class herbivore:
         self.age_to_fitness_death_ratio = self.age_to_fitness_death_ratio * (2 - self.will_die_of_age())
         return np.average(
             [self.will_die_of_age(), self.will_die_of_fitness()],
-            weights=[self.age_to_fitness_death_ratio, 1]
-            )
+            weights=[self.age_to_fitness_death_ratio, 1])
