2057ddf4ce729b4c515335543c3e2b8ead1744b7
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2019-11-15 03:59:17 +0530

    Added config file looking up

--- Cpp/RandomTests/test2.cpp
+++ Cpp/RandomTests/test2.cpp
@@ -1,25 +1,47 @@
-#include <string>
+#include <filesystem>
 #include <iostream>
-#include <random>
-
-std::string random_name(const int& inp)
-{
-    std::string str = "";
-    std::uniform_int_distribution<int> dis(0, 35);
-    std::mt19937_64 rng; rng.seed(std::random_device()());
-    for(int i = 0; i < inp; i++)
-    {
-        int tmp = dis(rng);
-        if(tmp >= 0 && tmp <= 9)
-            str += std::to_string(tmp);
-        else
-            str += (char)(tmp - 10 + 'a');
-    }
-    return str;
-}
+#include <string>
+#include <utility>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 int main()
 {
-    int n; std::cin >> n;
-    std::cout << random_name(n) << '\n';
+    std::filesystem::path p = std::filesystem::current_path();
+    auto logger = spdlog::stdout_color_mt("logger");
+    logger->set_pattern("%^[%n] %l: %v%$");
+    logger->info("Searching for ecosystem config.json in "+p.string());
+    bool config_dir_found = false, config_file_found = false;
+    auto func = [&](){
+        if( std::filesystem::exists(p / std::filesystem::path("config")))
+        {
+            logger->info("config directory found");
+            config_dir_found = true;
+            if(std::filesystem::exists(p / "config/config.json"))
+            {
+                logger->info("config.json found in " + p.string() + "/config");
+                config_file_found = true;
+            }
+            else
+            {
+                logger->warn("config.json not found in " + p.string() + "/config");
+            }
+        }
+    };
+    func();
+    if(!config_file_found)
+    {
+        if(!config_dir_found)
+            logger->warn("config directory not found in " + p.string());
+        p = p.parent_path();
+        logger->info("looking in " + p.string());
+        func();
+        if(!config_dir_found)
+        {
+            logger->warn("config directory not found in " + p.string());
+            logger->critical("No config.json found. Exiting");
+        }
+        else if(!config_file_found)
+            logger->critical("No config.json found. Exiting");
+    }
 }
