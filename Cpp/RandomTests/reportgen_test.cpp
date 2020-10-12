0019ac83a64a5e20a06db5e8da4c3a3603dc5c66
Sayan <sayan227sil@ymail.com>
2020-10-12 22:30:14 +0530

    Generate reports by github actions

---
+++ Cpp/RandomTests/reportgen_test.cpp
@@ -0,0 +1,23 @@
+#include <god.hpp>
+#include <iostream>
+
+int main()
+{
+    unsigned int initial_organism_count = 200;
+    unsigned int years_to_simulate = 100;
+
+    God allah(true);
+    allah.reset_species("animal/deer");
+    while (initial_organism_count--)
+    {
+        allah.spawn_organism(std::make_shared<Animal>("deer", 10));
+    }
+
+    while (years_to_simulate--)
+    {
+        allah.happy_new_year();
+        allah.remember_species("animal/deer");
+    }
+
+    print("Simulation complete. Ready to generate report.")
+}
