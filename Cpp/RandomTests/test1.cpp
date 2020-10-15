ab556daa1a50a928297a45b370b8dadae37b75e5
Sayan <sayan227sil@ymail.com>
2020-10-15 20:11:15 +0530

    Fix wierd average data in db and starting age of newborn

--- Cpp/RandomTests/test1.cpp
+++ Cpp/RandomTests/test1.cpp
@@ -1,61 +1,51 @@
 #include <god.hpp>
 #include <iostream>
-#include <memory>
+#include <helper.hpp>
 #include <stat_fetcher.hpp>
-
-void evaluation(const God &god)
-{
-    std::cout << "Population: " << stat_fetcher::get_population(god.organisms) << '\n';
-    std::cout << "Deaths: " << god.recent_deaths << '\n';
-    std::cout << "Births: " << god.recent_births << '\n';
-
-    std::cout << "Average height: " << stat_fetcher::get_stat_average(god.organisms, "height") << '\n';
-    std::cout << "Average weight: " << stat_fetcher::get_stat_average(god.organisms, "weight") << '\n';
-
-    double low, high;
-    std::string attribute;
-
-    attribute = "age";
-    std::tie(low, high) = stat_fetcher::get_stat_gap(god.organisms, attribute);
-    std::cout << attribute << " - Lowest: " << low << " | Highest: " << high << '\n';
-
-    attribute = "generation";
-    std::tie(low, high) = stat_fetcher::get_stat_gap(god.organisms, "generation");
-    std::cout << attribute << " - Lowest: " << low << " | Highest: " << high << '\n';
-
-    std::cout << "Kind Distribution:\n";
-    for (const auto &kind : stat_fetcher::get_kind_distribution(god.organisms))
-    {
-        std::cout << kind.first << " : " << kind.second << '\n';
-    }
-
-    std::cout << "\n\n";
-}
-
+#include <vector>
+#include <algorithm>
 int main()
 {
-    unsigned int initial_organism_count = 500;
-    unsigned int years_to_simulate = 1000;
+    unsigned int initial_organism_count = 100;
+    unsigned int years_to_simulate = 10;
 
-    God allah;
-    allah.reset_species("animal/deer");
+    God allah(true);
+    allah.reset_species("plant/bamboo");
 
     while (initial_organism_count--)
     {
-        allah.spawn_organism(std::make_shared<Animal>("deer", 10, true));
+        allah.spawn_organism(std::make_shared<Plant>("bamboo", 10));
     }
 
-    std::cout << "\n\nINITIAL EVALUATION:\n\n";
-    evaluation(allah);
-
-    std::cout << "Simulating " << years_to_simulate << " years\n";
+//    std::vector<std::vector<std::string>> data;
+//    std::vector<std::string> avg_data;
+//    std::vector<PStat> temp;
+//    std::vector<std::string> temp2;
+    int k = 0;
 
     while (years_to_simulate--)
     {
         allah.happy_new_year(true);
-        allah.remember_species("animal/deer");
+        allah.remember_species("plant/bamboo");
+
+        int l, h;
+        std::tie(l, h) = stat_fetcher::get_stat_gap(allah.organisms, "age", "bamboo");
+        std::cout << "Low: " << l << ", High: " << h << "\n";
+//        avg_data.push_back(std::to_string(stat_fetcher::get_stat_average(allah.organisms, "age", "bamboo")));
+//        temp = stat_fetcher::get_one_stat(allah.organisms, "age", "bamboo");
+//        std::transform(temp.begin(), temp.end(), std::back_inserter(temp2), [](PStat p){return p.getString();});
+//        data.push_back(temp2);
+//        temp.clear();
+//        temp2.clear();
     }
 
-    std::cout << "\n\nFINAL EVALUATION:\n\n";
-    evaluation(allah);
+//    for (auto temp: data)
+//    {
+//        std::cout << "Average: " << avg_data[k] << ", after " << k++ << " years:\n";
+//        for (auto t: temp)
+//        {
+//            std::cout << t << ", ";
+//        }
+//        std::cout << "\n\n";
+//    }
 }
