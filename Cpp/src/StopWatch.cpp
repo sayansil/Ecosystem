cb06f8d09e98d50ddf63368cec584757e5ad8f18
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2020-11-04 14:31:09 +0530

    Cause I like it that way

--- Cpp/src/StopWatch.cpp
+++ Cpp/src/StopWatch.cpp
@@ -10,12 +10,10 @@ namespace timer
 
     void StopWatch::stop()
     {
-        // avg(n+1) = (n / (n + 1)) * avg(n) + (1 / (n + 1)) * kn
         status = false;
         t_end = std::chrono::steady_clock::now();
         cur_time = std::chrono::duration_cast<std::chrono::nanoseconds>(t_end - t_start).count();
-        //avg_time = ( avg_time * count + cur_time ) / (count + 1);
-        avg_time = ((double)count / (count + 1)) * avg_time + (1.0 / (count + 1)) * cur_time;
+        avg_time = ( avg_time * count + cur_time ) / (count + 1);
         count++;
     }
 
