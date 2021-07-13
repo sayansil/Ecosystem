1da3e9836baf5c0b0016dbea539f0ef062bf8350
Sayan <sayan227sil@ymail.com>
2021-07-13 22:27:36 +0530

    Implots was not guilty

--- Cpp/run/main.cpp
+++ Cpp/run/main.cpp
@@ -22,8 +22,8 @@ int main(int, char**)
 {
     // ------------------ Window creation and handling starts  ------------------
 
-    int size_X = 1400;
-    int size_Y = 720;
+    int size_X = 1920;
+    int size_Y = 1080;
 
     unsigned int initial_organism_count = 200;
     unsigned int years_to_simulate = 200;
@@ -42,10 +42,17 @@ int main(int, char**)
     int k = years_to_simulate;
 
     std::vector<int> population;
-    int max_population = 0;
     std::vector<int> avg_age;
     std::vector<double> avg_height;
     std::vector<double> avg_weight;
+    int max_population = 0;
+    int max_age = 0;
+    double max_height = 0;
+    double max_weight = 0;
+    int min_population = 0;
+    int min_age = 0;
+    double min_height = 0;
+    double min_weight = 0;
 
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit())
@@ -107,19 +114,65 @@ int main(int, char**)
         ImGui::Text("Species: %s", species.c_str());
         ImGui::Text("Initial Population: %d", initial_organism_count);
         ImGui::Text("Years to simulate: %d", years_to_simulate);
-        ImGui::Dummy(ImVec2(0.0f, 10.0f));
+        ImGui::Dummy(ImVec2(0.0f, 20.0f));
 
         if (started)
         {
+            ImGui::Text("Current Year of Simulation: %d", years_to_simulate - k);
+            ImGui::Dummy(ImVec2(0.0f, 20.0f));
+
             std::vector<int> x(years_to_simulate - k);
             std::iota(x.begin(), x.end(), 1);
 
-            ImPlot::SetNextPlotLimits(1, years_to_simulate, 0, max_population, ImGuiCond_Always);
-            if(ImPlot::BeginPlot("Population"))
+            if (ImGui::BeginTable("Plots", 2))
             {
-                ImPlot::PushColormap(ImPlotColormap_Twilight);
-                ImPlot::PlotLine("Line Plot", x.data(), population.data(), x.size());
-                ImPlot::EndPlot();
+                ImGui::TableNextRow();
+                ImGui::TableSetColumnIndex(0);
+
+                // Table -(0, 0)
+                ImPlot::SetNextPlotLimits(1, years_to_simulate, min_population, max_population, ImGuiCond_Always);
+                if(ImPlot::BeginPlot("Population"))
+                {
+                    ImPlot::PushColormap(ImPlotColormap_Twilight);
+                    ImPlot::PlotLine("", x.data(), population.data(), x.size());
+                    ImPlot::EndPlot();
+                }
+
+                ImGui::TableSetColumnIndex(1);
+
+                // Table -(0, 1)
+                ImPlot::SetNextPlotLimits(1, years_to_simulate, min_age, max_age, ImGuiCond_Always);
+                if(ImPlot::BeginPlot("Average Age"))
+                {
+                    ImPlot::PushColormap(ImPlotColormap_Twilight);
+                    ImPlot::PlotLine("", x.data(), avg_age.data(), x.size());
+                    ImPlot::EndPlot();
+                }
+
+                ImGui::TableNextRow();
+                ImGui::TableSetColumnIndex(0);
+
+                // Table -(1, 0)
+                ImPlot::SetNextPlotLimits(1, years_to_simulate, min_height, max_height, ImGuiCond_Always);
+                if(ImPlot::BeginPlot("Average Height"))
+                {
+                    ImPlot::PushColormap(ImPlotColormap_Twilight);
+                    ImPlot::PlotLine("", (double*)x.data(), avg_height.data(), x.size());
+                    ImPlot::EndPlot();
+                }
+
+                ImGui::TableSetColumnIndex(1);
+
+                // Table -(1, 1)
+                ImPlot::SetNextPlotLimits(1, years_to_simulate, min_weight, max_weight, ImGuiCond_Always);
+                if(ImPlot::BeginPlot("Average Weight"))
+                {
+                    ImPlot::PushColormap(ImPlotColormap_Twilight);
+                    ImPlot::PlotLine("", (double*)x.data(), avg_weight.data(), x.size());
+                    ImPlot::EndPlot();
+                }
+
+                ImGui::EndTable();
             }
         }
         else
@@ -147,10 +200,17 @@ int main(int, char**)
         {
             allah.happy_new_year(false);
             population.push_back(stat_fetcher::get_population(allah.organisms));
-            max_population = population.back() > max_population ? population.back() : max_population;
-            avg_age.push_back(stat_fetcher::get_population(allah.organisms));
+            avg_age.push_back(stat_fetcher::get_stat_average(allah.organisms, "age"));
             avg_height.push_back(stat_fetcher::get_stat_average(allah.organisms, "height"));
             avg_weight.push_back(stat_fetcher::get_stat_average(allah.organisms, "weight"));
+            max_population = population.back() > max_population ? population.back() : max_population;
+            max_age = avg_age.back() > max_age ? avg_age.back() : max_age;
+            max_height = avg_height.back() > max_height ? avg_height.back() : max_height;
+            max_weight = avg_weight.back() > max_weight ? avg_weight.back() : max_weight;
+            min_population = population.back() < min_population || k == years_to_simulate ? population.back() : min_population;
+            min_age = avg_age.back() < min_age || k == years_to_simulate ? avg_age.back() : min_age;
+            min_height = avg_height.back() > min_height || k == years_to_simulate ? avg_height.back() : min_height;
+            min_weight = avg_weight.back() > min_weight || k == years_to_simulate ? avg_weight.back() : min_weight;
             k--;
         }
     }
