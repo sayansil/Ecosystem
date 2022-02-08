dc2bec8e95844e1827804350b87a57e1fbe2ba1f
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2022-02-09 00:31:27 +0530

    Maine kar diya

--- Cpp/RandomTests/test1.cpp
+++ Cpp/RandomTests/test1.cpp
@@ -2,6 +2,8 @@
 #include <fmt/core.h>
 #include <vector>
 #include <species_report_generated.h>
+#include <nlohmann/json.hpp>
+#include <flatbuffers/minireflect.h>
 
 int main()
 {
@@ -9,36 +11,61 @@ int main()
     Visualisation::SpeciesReportBuilder report_builder(builder);
 
     std::vector<flatbuffers::Offset<Visualisation::MultiPlot>> stdvecMultiplot;
-    Visualisation::MultiPlotBuilder multiplot_builder(builder);
-
-    std::vector<flatbuffers::Offset<Visualisation::SinglePlot>> stdvecSingleplot;
-    Visualisation::SinglePlotBuilder singleplot_builder(builder);
 
     std::vector<float> tmp = {0, 1, 2};
 
-    multiplot_builder.add_title(builder.CreateString("Theoretical max attributes"));
-    singleplot_builder.add_title(builder.CreateString("Theoretical max height"));
-    singleplot_builder.add_x(builder.CreateVector(tmp));
-    singleplot_builder.add_y(builder.CreateVector(tmp));
-    stdvecSingleplot.push_back(singleplot_builder.Finish());
-    singleplot_builder.add_title(builder.CreateString("Theoretical max width"));
-    singleplot_builder.add_x(builder.CreateVector(tmp));
-    singleplot_builder.add_y(builder.CreateVector(tmp));
-    stdvecSingleplot.push_back(singleplot_builder.Finish());
-    multiplot_builder.add_plots(builder.CreateVector(stdvecSingleplot));
-    stdvecMultiplot.push_back(multiplot_builder.Finish());
+    {
+        Visualisation::MultiPlotBuilder multiplot_builder(builder);
+        std::vector<flatbuffers::Offset<Visualisation::SinglePlot>> stdvecSingleplot;
 
-    multiplot_builder.add_title(builder.CreateString("Theoretical max attributes 2"));
-    singleplot_builder.add_title(builder.CreateString("Theoretical max height"));
-    singleplot_builder.add_x(builder.CreateVector(tmp));
-    singleplot_builder.add_y(builder.CreateVector(tmp));
-    stdvecSingleplot.push_back(singleplot_builder.Finish());
-    singleplot_builder.add_title(builder.CreateString("Theoretical max width"));
-    singleplot_builder.add_x(builder.CreateVector(tmp));
-    singleplot_builder.add_y(builder.CreateVector(tmp));
-    stdvecSingleplot.push_back(singleplot_builder.Finish());
-    multiplot_builder.add_plots(builder.CreateVector(stdvecSingleplot));
-    stdvecMultiplot.push_back(multiplot_builder.Finish());
+        // first single plot
+        {
+            Visualisation::SinglePlotBuilder singleplot_builder(builder);
+            singleplot_builder.add_title(builder.CreateString("Theoretical max height"));
+            singleplot_builder.add_x(builder.CreateVector(tmp));
+            singleplot_builder.add_y(builder.CreateVector(tmp));
+            stdvecSingleplot.push_back(singleplot_builder.Finish());
+        }
+
+        // second single plot
+        {
+            Visualisation::SinglePlotBuilder singleplot_builder(builder);
+            singleplot_builder.add_title(builder.CreateString("Theoretical max width"));
+            singleplot_builder.add_x(builder.CreateVector(tmp));
+            singleplot_builder.add_y(builder.CreateVector(tmp));
+            stdvecSingleplot.push_back(singleplot_builder.Finish());
+        }
+
+        // create first multiplot
+        multiplot_builder.add_plots(builder.CreateVector(stdvecSingleplot));
+        multiplot_builder.add_title(builder.CreateString("Theoretical max attributes"));
+        stdvecMultiplot.push_back(multiplot_builder.Finish());
+    }
+
+    {
+        Visualisation::MultiPlotBuilder multiplot_builder(builder);
+        std::vector<flatbuffers::Offset<Visualisation::SinglePlot>> stdvecSingleplot;
+        
+        {
+            Visualisation::SinglePlotBuilder singleplot_builder(builder);
+            singleplot_builder.add_title(builder.CreateString("Theoretical max height"));
+            singleplot_builder.add_x(builder.CreateVector(tmp));
+            singleplot_builder.add_y(builder.CreateVector(tmp));
+            stdvecSingleplot.push_back(singleplot_builder.Finish());
+        }
+        
+        {
+            Visualisation::SinglePlotBuilder singleplot_builder(builder);
+            singleplot_builder.add_title(builder.CreateString("Theoretical max width"));
+            singleplot_builder.add_x(builder.CreateVector(tmp));
+            singleplot_builder.add_y(builder.CreateVector(tmp));
+            stdvecSingleplot.push_back(singleplot_builder.Finish());
+        }
+        
+        multiplot_builder.add_plots(builder.CreateVector(stdvecSingleplot));
+        multiplot_builder.add_title(builder.CreateString("Theoretical max attributes 2"));
+        stdvecMultiplot.push_back(multiplot_builder.Finish());
+    }
 
     report_builder.add_title(builder.CreateString("My report"));
     report_builder.add_species(builder.CreateString("Human"));
@@ -49,4 +76,8 @@ int main()
     builder.Clear();
 
     fmt::print("Buffer size: {}\n", buffer.size());
+    flatbuffers::ToStringVisitor visitor("", true, "", true);
+    flatbuffers::IterateFlatBuffer(buffer.data(), Visualisation::SpeciesReportTypeTable(), &visitor);
+    nlohmann::json json_data = nlohmann::json::parse(visitor.s);
+    fmt::print("Parsed JSON:\n{}\n", json_data.dump(4));
 }
