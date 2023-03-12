4e19fd7fcf8e4b99942ff564bf762c16c9ef17f9
Sayan Sil <sayan227sil@ymail.com>
2023-03-12 05:47:42 +0530

    Fix lint

--- simulation/src/ecosystem_wrapper.cpp
+++ simulation/src/ecosystem_wrapper.cpp
@@ -12,24 +12,24 @@ God *god;
 GodState godState = GodState::unborn;
 
 void create_god(uint8_t gods_eye, const char *ecosystem_root) {
-  if (godState == GodState::unborn || godState == GodState::dead) {
-    setup::setup(ecosystem_root);
-    god = new God(ecosystem_root, gods_eye);
-    godState = GodState::born;
-  }
+    if (godState == GodState::unborn || godState == GodState::dead) {
+        setup::setup(ecosystem_root);
+        god = new God(ecosystem_root, gods_eye);
+        godState = GodState::born;
+    }
 }
 
 void set_initial_organisms(uint32_t kingdom, const char *kind, uint32_t age,
                            uint32_t count) {
-  if (godState == GodState::born) {
-    organisms.reserve(organisms.capacity() + count);
+    if (godState == GodState::born) {
+        organisms.reserve(organisms.capacity() + count);
 
-    for (size_t i = 0; i < count; i++) {
-      organisms.push_back({{"kind", std::string(kind)},
-                           {"kingdom", std::to_string(kingdom)},
-                           {"age", std::to_string(age)}});
+        for (size_t i = 0; i < count; i++) {
+            organisms.push_back({{"kind", std::string(kind)},
+                                 {"kingdom", std::to_string(kingdom)},
+                                 {"age", std::to_string(age)}});
+        }
     }
-  }
 }
 
 void clean_slate() {
